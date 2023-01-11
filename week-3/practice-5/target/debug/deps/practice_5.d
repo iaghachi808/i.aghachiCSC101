0082a0f0005faa3a6ad67fbcff9a75662bdc0d40
iaghachi808 <115728447+iaghachi808@users.noreply.github.com>
2023-01-11 14:40:13 -0800

    Delete week-3/practice-5/target directory

--- week-3/practice-5/target/debug/deps/practice_5.d
+++
@@ -1,5 +0,0 @@
-C:\Users\IKE\Documents\i.aghachiCSC101\week-3\practice-5\target\debug\deps\practice_5.exe: src\main.rs
-
-C:\Users\IKE\Documents\i.aghachiCSC101\week-3\practice-5\target\debug\deps\practice_5.d: src\main.rs
-
-src\main.rs:
